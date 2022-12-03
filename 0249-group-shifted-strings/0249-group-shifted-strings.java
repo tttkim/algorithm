38663dda6008bccffb8597d53629f99eff1acba0
tttkim <rlaxogk006@naver.com>
2022-12-03 21:01:03 +0900

    Time: 3 ms (84.70%), Space: 43.7 MB (65.95%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,26 +1,41 @@
 class Solution {
+    char shiftLetter(char letter, int shift) {
+        return (char) ((letter - shift + 26) % 26 + 'a');
+    }
+    
+    // Create a hash value
+    String getHash(String s) {
+        char[] chars = s.toCharArray();
+        
+        // Calculate the number of shifts to make the first character to be 'a'
+        int shift = chars[0];
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = shiftLetter(chars[i], shift);
+        }
+        
+        String hashKey = String.valueOf(chars);
+        return hashKey;
+    }
+    
     public List<List<String>> groupStrings(String[] strings) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String s : strings) {
-            char first = s.charAt(0);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < s.length(); i++) {
-                char curr = s.charAt(i);
-                int offset = curr - first;
-                if (offset < 0)
-                    offset += 26;
-                sb.append(Integer.toString(offset));
-                sb.append(' ');
-            }
-            if (!map.containsKey(sb.toString())) {
-                map.put(sb.toString(), new ArrayList<String>());
-            }
-            map.get(sb.toString()).add(s);
+        Map<String, List<String>> mapHashToList = new HashMap<>();
+        
+        // Create a hash_value (hashKey) for each string and append the string
+        // to the list of hash values i.e. mapHashToList["abc"] = ["abc", "bcd"]
+        for (String str : strings) {
+            String hashKey = getHash(str);
+            if (mapHashToList.get(hashKey) == null) {
+                mapHashToList.put(hashKey, new ArrayList<>());
+            } 
+            mapHashToList.get(hashKey).add(str);
         }
-        List<List<String>> answer = new ArrayList<>();
-        for (String key : map.keySet()) {
-            answer.add(map.get(key));
+        
+        // Iterate over the map, and add the values to groups
+        List<List<String>> groups = new ArrayList<>();
+        for (List<String> group : mapHashToList.values()) {
+            groups.add(group);
         }
-        return answer;
+        
+        return groups;
     }
 }
