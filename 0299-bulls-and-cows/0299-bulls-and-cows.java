00ca22daaefb9f424bac8d0ef7119ba922535176
tttkim <rlaxogk006@naver.com>
2022-12-04 11:49:00 +0900

    Time: 8 ms (83.21%), Space: 42.5 MB (78.24%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -10,34 +10,21 @@ class Solution {
         }
         int bull = 0;
         int cow = 0;
-        boolean[] checkList = new boolean[guess.length()];
         for (int i = 0; i < guess.length(); i++) {
-            char s = secret.charAt(i);
             char g = guess.charAt(i);
-            if (s == g) {
-                bull++;
-                checkList[i] = true;
-                if (secretMap.get(s) == 1) {
-                    secretMap.remove(s);
+            if (secretMap.containsKey(g)) {
+                if (g == secret.charAt(i)) {
+                    bull++;
+                    if (secretMap.get(g) <= 0)
+                        cow--;
                 } else {
-                    secretMap.put(s, secretMap.get(s)-1);
+                    if (secretMap.get(g) > 0)
+                        cow++;
                 }
+                secretMap.put(g, secretMap.get(g)-1);
             }
         }
-        for (int i = 0; i < guess.length(); i++) {
-            if (!checkList[i]) {
-                char g = guess.charAt(i);
-                if (secretMap.containsKey(g)) {
-                    cow++;
-                    if (secretMap.get(g) == 1) {
-                        secretMap.remove(g);
-                    } else {
-                        secretMap.put(g, secretMap.get(g)-1);
-                    }
-                }
-            }
-            
-        }
+        
         return bull + "A" + cow + "B";
     }
 }
